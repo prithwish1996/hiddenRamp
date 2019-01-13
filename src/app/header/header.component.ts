77ecffa5207b22b0e9f9705a700ec7d2514ed0fa
Prithwish <prithwish.dsgpt@gmail.com>
2019-01-13 23:01:04 +0530

    15th upload

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AppComponent} from '../app.component';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public app: AppComponent) { }
 
   ngOnInit() {
   }
