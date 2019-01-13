c3d8987e10e71299fcfe0a684e3836066e6da3c2
Prithwish <prithwish.dsgpt@gmail.com>
2019-01-13 21:21:19 +0530

    13th upload

--- src/app/view-testimonial/view-testimonial.component.ts
+++ src/app/view-testimonial/view-testimonial.component.ts
@@ -27,8 +27,8 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  var slides = document.getElementsByClassName("mySlides") as HTMLCollectionOf<HTMLElement>;
+  var dots = document.getElementsByClassName("dot") as HTMLCollectionOf<HTMLElement>;
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
