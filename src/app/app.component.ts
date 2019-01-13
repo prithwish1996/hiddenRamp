3dd489d77252d19cedf861aa3f535aba36c645aa
Prithwish <prithwish.dsgpt@gmail.com>
2019-01-14 00:14:30 +0530

    17th upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
+import { Event, Router, NavigationStart, NavigationEnd} from '@angular/router';
 
 import * as $ from 'jquery';
 
@@ -12,30 +13,58 @@ import * as $ from 'jquery';
 export class AppComponent {
   title = 'hiddenRampManagement';
 
-  isPressed=false;
-  constructor(public spinner: NgxSpinnerService, public spinnerService: Ng4LoadingSpinnerService){}
-
- 
-
-  	myStyle: object = {};
+    myStyle: object = {};
     myParams: object = {};
     width: number = 100;
     height: number = 100;
+     showLoading:boolean = true;
     template: string =
 
 `<img class="custom-spinner-template" src="http://pa1.narvii.com/5722/2c617cd9674417d272084884b61e4bb7dd5f0b15_hq.gif">`
+
+  isPressed=false;
+
+  constructor(public spinner: NgxSpinnerService, public spinnerService: Ng4LoadingSpinnerService, public _router: Router){
+
+   // this.showLoading = true;
+     this.spinnerService.show();
+
+    this._router.events.subscribe((routerEvent: Event)=>{
+
+      if(routerEvent instanceof NavigationStart){
+
+        // this.showLoading = true;
+        // console.log(this.showLoading);
+         this.spinnerService.show();
+
+      }
+
+      if(routerEvent instanceof NavigationEnd){
+
+          // this.showLoading = true;
+          // console.log(this.showLoading);
+          this.spinnerService.hide();
+
+      }
+    })
+
+  }
+
+ 
+
+  
 
     ngOnInit() {
 
     	
 
 
-    	 this.spinnerService.show();
+    // 	 this.spinnerService.show();
  
-    setTimeout(() => {
-        /** spinner ends after 5 seconds */
-        this.spinnerService.hide();
-    }, 5000);
+    // setTimeout(() => {
+    //     * spinner ends after 5 seconds 
+    //     this.spinnerService.hide();
+    // }, 5000);
         this.myStyle = {
             'position': 'fixed',
             'width': '100%',
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
 import {AppComponent} from '../app.component';
+import { Event, Router, NavigationStart, NavigationEnd} from '@angular/router';
 import * as $ from 'jquery';
 
 @Component({
@@ -11,25 +12,51 @@ import * as $ from 'jquery';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(public spinner: NgxSpinnerService, public spinnerService: Ng4LoadingSpinnerService, public app: AppComponent) { }
-
-  myStyle: object = {};
+	myStyle: object = {};
     myParams: object = {};
     width: number = 100;
     height: number = 100;
+    // isLoading = false;
     template: string =
 
 `<img class="custom-spinner-template" src="http://pa1.narvii.com/5722/2c617cd9674417d272084884b61e4bb7dd5f0b15_hq.gif">`
 
+  constructor(public spinner: NgxSpinnerService, public spinnerService: Ng4LoadingSpinnerService, public app: AppComponent, public _router: Router) {
+
+  		// this.isLoading = true;
+		// this.spinnerService.show();
+    // this._router.events.subscribe((routerEvent: Event)=>{
+
+    //   if(routerEvent instanceof NavigationStart){
+
+
+    //   	this.isLoading = true;
+    //      // this.spinnerService.hide();
+
+    //   }
+
+    //   if(routerEvent instanceof NavigationEnd){
+
+    //   	this.isLoading = false;
+    //       // this.spinnerService.hide();
+
+    //   }
+    // })
+
+
+   }
+
+  
+
   ngOnInit() {
 
   	this.app.isPressed = false;
-  	 this.spinnerService.show();
+  	 // this.spinnerService.show();
  
-    setTimeout(() => {
-        /** spinner ends after 5 seconds */
-        this.spinnerService.hide();
-    }, 5000);
+    // setTimeout(() => {
+    //     // * spinner ends after 5 seconds 
+    //     this.spinnerService.hide();
+    // }, 5000);
         this.myStyle = {
             'position': 'fixed',
             'width': '100%',
